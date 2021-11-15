dfa2efeba8d83e259addfb19f59f3665da339c46
guoxudong <guoxudong.dev@gmail.com>
2021-11-15 10:28:38 +0800

    update completion cmd

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ import (
 	"os"
 
 	"github.com/sunny0826/kubecm/cmd"
-	//_ "k8s.io/client-go/plugin/pkg/client/auth/azure" // required for Azure
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" // required for GKE
+	_ "k8s.io/client-go/plugin/pkg/client/auth/azure" // required for Azure
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"   // required for GKE
 )
 
 func main() {
