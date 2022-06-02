73350cf48cb9a4fa4a50f4304e854f11a7e102c2
guoxudong <guoxudong.dev@gmail.com>
2022-06-02 08:15:42 +0800

    fix

--- hack/big.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/pterm/pterm"
-
-func main() {
-	s, _ := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString("guoxudong.io")).Srender()
-	pterm.Println(s) // Print BigLetters with the default CenterPrinter
-}
