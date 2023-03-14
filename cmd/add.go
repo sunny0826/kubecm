2531e3d4f8e3909022ae50aad3e4f2318c9362a7
AdamRussak <adamrussak@gmail.com>
2023-03-14 19:19:29 +0000

    test without else in add3

--- cmd/add.go
+++ cmd/add.go
@@ -115,6 +115,8 @@ func (kc *KubeConfigOption) handleContexts(oldConfig *clientcmdapi.Config) (*cli
 		var newName string
 		if len(kc.config.Contexts) > 1 {
 			newName = fmt.Sprintf("%s-%s", kc.fileName, HashSufString(name))
+		} else {
+			newName = name
 		}
 		if checkContextName(newName, oldConfig) {
 			nameConfirm := BoolUI(fmt.Sprintf("「%s」 Name already exists, do you want to rename it. (If you select `False`, this context will not be merged)", newName))
