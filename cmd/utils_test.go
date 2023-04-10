f1e7727274754af035c8c6d974eddfb3694d7f23
guoxudong <guoxudong.dev@gmail.com>
2023-04-10 16:44:34 +0800

    fix

--- cmd/utils_test.go
+++ cmd/utils_test.go
@@ -225,31 +225,31 @@ func TestCheckAndTransformFilePath(t *testing.T) {
 	}
 }
 
-//func TestCheckValidContext(t *testing.T) {
-//	clearWrongConfig := wrongFederalConfig.DeepCopy()
-//	clearWrongWant := appendRootConfigConflictAlfa.DeepCopy()
-//	type args struct {
-//		clear  bool
-//		config *clientcmdapi.Config
-//	}
-//	tests := []struct {
-//		name string
-//		args args
-//		want *clientcmdapi.Config
-//	}{
-//		// TODO: Add test cases.
-//		{"check-root", args{clear: false, config: &wrongRootConfig}, &appendConfigAlfa},
-//		{"check-federal", args{clear: false, config: &wrongFederalConfig}, &appendRootConfigConflictAlfa},
-//		{"clear-federal", args{clear: true, config: clearWrongConfig}, clearWrongWant},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			if got := CheckValidContext(tt.args.clear, tt.args.config); !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("CheckValidContext() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
+func TestCheckValidContext(t *testing.T) {
+	clearWrongConfig := wrongFederalConfig.DeepCopy()
+	clearWrongWant := appendRootConfigConflictAlfa.DeepCopy()
+	type args struct {
+		clear  bool
+		config *clientcmdapi.Config
+	}
+	tests := []struct {
+		name string
+		args args
+		want *clientcmdapi.Config
+	}{
+		// TODO: Add test cases.
+		{"check-root", args{clear: false, config: &wrongRootConfig1}, &appendConfigAlfa},
+		{"check-federal", args{clear: false, config: &wrongFederalConfig}, &appendRootConfigConflictAlfa},
+		{"clear-federal", args{clear: true, config: clearWrongConfig}, clearWrongWant},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CheckValidContext(tt.args.clear, tt.args.config); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CheckValidContext() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func checkConfig(want, got *clientcmdapi.Config, t *testing.T) {
 	if !apiequality.Semantic.DeepEqual(want, got) {
@@ -339,27 +339,38 @@ func TestSelectUI(t *testing.T) {
 	}
 }
 
-type testPromptUI struct {
+type testPrompt struct {
 	result string
 	err    error
 }
 
-func (t *testPromptUI) Run() (string, error) {
+func (t *testPrompt) Run() (string, error) {
 	return t.result, t.err
 }
 
 func TestPromptUI(t *testing.T) {
 	tests := []struct {
 		name        string
-		prompt      *testPromptUI
+		prompt      *testPrompt
 		label       string
 		defaultName string
 		expected    string
 		expectErr   bool
 	}{
 		{
+			name: "Valid input",
+			prompt: &testPrompt{
+				result: "TestName",
+				err:    nil,
+			},
+			label:       "Enter name",
+			defaultName: "DefaultName",
+			expected:    "TestName",
+			expectErr:   false,
+		},
+		{
 			name: "Error occurred",
-			prompt: &testPromptUI{
+			prompt: &testPrompt{
 				result: "",
 				err:    errors.New("Prompt failed"),
 			},
